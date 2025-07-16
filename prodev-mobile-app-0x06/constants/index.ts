78cbf06ba51ab623e17428719caa82ddc4e15ee9
hamazin <hademolahamazin@gmail.com>
2025-07-16 18:19:59 +0100

    Added prodev-mobile-app-0x06

---
+++ prodev-mobile-app-0x06/constants/index.ts
@@ -0,0 +1,9 @@
+const BACKGROUNDIMAGE = require('@/assets/images/hero-icon.png')
+const HEROLOGO =require('@/assets/images/hero-logo.png')
+const HEROLGOGREEN = require('@/assets/images/logo-green.png')
+const GOOGLELOGO = require('@/assets/images/google.png')
+const FACEBOOKLOGO = require('@/assets/images/facebook.png')
+
+export {
+    BACKGROUNDIMAGE, FACEBOOKLOGO, GOOGLELOGO, HEROLGOGREEN, HEROLOGO
+}
